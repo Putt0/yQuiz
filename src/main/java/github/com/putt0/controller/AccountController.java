1a86e026d39ea8aeb7174942ea4441162c27cb5c
Reinaldy Oliveira <reinaldymc4@gmail.com>
2023-08-03 12:20:52 -0300

    AccountController class from Account

---
+++ src/main/java/github/com/putt0/controller/AccountController.java
@@ -0,0 +1,36 @@
+package github.com.putt0.controller;
+
+import github.com.putt0.account.Account;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class AccountController {
+
+    private final Map<UUID, Account> ACCOUNT_MAP;
+
+    public AccountController() {
+        ACCOUNT_MAP = new HashMap<>();
+    }
+
+    public void load(Account account) {
+        ACCOUNT_MAP.put(account.getUniqueId(), account);
+    }
+
+    public void remove(UUID uniqueId) {
+        ACCOUNT_MAP.remove(uniqueId);
+    }
+
+    public void update(Account account) {
+        ACCOUNT_MAP.putIfAbsent(account.getUniqueId(), account);
+    }
+
+    public Account get(UUID uniqueId) {
+        return ACCOUNT_MAP.get(uniqueId);
+    }
+
+    public Account get(String name) {
+        return ACCOUNT_MAP.values().stream().filter(account -> account.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+    }
+}
